fabdf9dcb78c41f5dff0c2df6f03551c6f5e3d52
Baptiste Brassart <brassart.baptiste@gmail.com>
2015-11-30 18:26:10 +0100

    Trying to fix Bin

--- config/application.rb
+++ config/application.rb
@@ -8,15 +8,20 @@ require "active_job/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
-#require "sprockets/railtie"
+require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
+
 module Bbrassart
   class Application < Rails::Application
+    config.generators do |g|
+      g.orm :mongoid
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
