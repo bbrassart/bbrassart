d79c87d13d9bb32aa2a143b3b2c586020fdf8041
Baptiste Brassart <brassart.baptiste@gmail.com>
2015-11-30 16:16:50 +0100

    Fixing mongoid Heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
-Blog.destroy_all
 puts "Empty DB"
 
 Blog.create(
