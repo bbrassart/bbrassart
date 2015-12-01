19b3aa0376f203f103914a32057164ef6374e29a
Baptiste Brassart <brassart.baptiste@gmail.com>
2015-12-01 16:34:16 +0100

    Ready to fix navbar for mobile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,7 @@
 //= require_tree .
 
 $(document).on('ready page:load', function () {
+  $(".button-collapse").sideNav();
   var listenToGithub = Object.create(github);
   listenToGithub.setInitialListeners();
   var listenToBlog = Object.create(blog);
@@ -29,4 +30,43 @@ $(document).on('ready page:load', function () {
 
   var listenToCV = Object.create(cv);
   listenToCV.setInitialListeners();
+
+  (function (window, document) {
+    var menu = document.getElementById('menu'),
+        WINDOW_CHANGE_EVENT = ('onorientationchange' in window) ? 'orientationchange':'resize';
+
+    function toggleHorizontal() {
+        [].forEach.call(
+            document.getElementById('menu').querySelectorAll('.custom-can-transform'),
+            function(el){
+                el.classList.toggle('pure-menu-horizontal');
+            }
+        );
+    };
+
+    function toggleMenu() {
+        // set timeout so that the panel has a chance to roll up
+        // before the menu switches states
+        if (menu.classList.contains('open')) {
+            setTimeout(toggleHorizontal, 500);
+        }
+        else {
+            toggleHorizontal();
+        }
+        menu.classList.toggle('open');
+        document.getElementById('toggle').classList.toggle('x');
+    };
+
+    function closeMenu() {
+        if (menu.classList.contains('open')) {
+            toggleMenu();
+        }
+    }
+
+    document.getElementById('toggle').addEventListener('click', function (e) {
+        toggleMenu();
+    });
+
+    window.addEventListener(WINDOW_CHANGE_EVENT, closeMenu);
+    })(this, this.document);
 });
