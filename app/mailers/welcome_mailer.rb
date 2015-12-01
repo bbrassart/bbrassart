2d6786cad2b2faf99de5ce32fd1cdd4e04fa3ed9
Baptiste Brassart <brassart.baptiste@gmail.com>
2015-12-01 09:27:03 +0100

    Fixing mailer

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -2,6 +2,6 @@ class WelcomeMailer < ApplicationMailer
 
   def contact(message)
     @message = message
-    mail(to: 'brassart.baptiste@gmail.com', subject: '#{@message.name} you contacted through bbrassart.com')
+    mail(to: "brassart.baptiste@gmail.com", subject: "#{@message.name} you contacted through bbrassart.com")
   end
 end
