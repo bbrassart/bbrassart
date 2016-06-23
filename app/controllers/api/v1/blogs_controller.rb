96c15b605f6e845aedb08be1834fc6d8fe8f4a74
bbrassart <brassart.baptiste@gmail.com>
2016-06-23 15:51:30 +0200

    Added some caching

--- app/controllers/api/v1/blogs_controller.rb
+++ app/controllers/api/v1/blogs_controller.rb
@@ -1,12 +1,16 @@
 class Api::V1::BlogsController < ApplicationController
   def index
-    blogs = Blog.all
+    blogs = Rails.cache.fetch("blog/all") do
+      Blog.all
+    end
     render json: blogs
   end
 
   def show
     index = params[:index]
-    blog = Blog.find_by(currentIndex: index)
+    blog = Rails.cache.fetch("blog/#{params[:index]}") do
+      Blog.find_by(currentIndex: index)
+    end
     render json: blog
   end
 
--- app/controllers/api/v1/experiences_controller.rb
+++ app/controllers/api/v1/experiences_controller.rb
@@ -1,7 +1,9 @@
 class Api::V1::ExperiencesController < ApplicationController
   def all_experiences
-    year = Year.find_by(title: params[:year].to_i)
-    response = year.experiences
-    render json: response
+    year = Rails.cache.fetch("cv/#{params[:year]}") do
+      Year.find_by(title: params[:year].to_i)
+    end
+    xps = year.experiences
+    render json: xps
   end
 end
