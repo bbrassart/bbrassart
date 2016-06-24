c9d1ff2345ff9d293bfdb69b3d57e8bf1469f205
bbrassart <brassart.baptiste@gmail.com>
2016-06-24 18:35:15 +0200

    Precompiled assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,4 @@
+
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
@@ -15,45 +16,44 @@
 //= require turbolinks
 //= require_tree .
 (function() {
+    $(document).on('ready page:load', function () {
 
-  $(document).on('ready page:load', function () {
-    //anchor navigation scroll effect
-    $('a[href*=#]:not([href=#])').click(function() {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-          || location.hostname == this.hostname) {
+        //anchor navigation scroll effect
+        $('a[href*="#"]:not([href="#"])').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
 
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-              $('html,body').animate({
-                  scrollTop: target.offset().top
-              }, 1000);
-              return false;
-          }
-      }
-    });
 
-    if ($("#comments").length != 0) {
-      !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
-      !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+        if ($("#comments").length != 0) {
+            !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+            !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
 
-      $('.header').addClass("blog--header");
-    }
-    var listenToGithub = Object.create(github);
-    listenToGithub.setInitialListeners();
-    var listenToBlog = Object.create(blog);
-    listenToBlog.buildFirstPost();
-    listenToBlog.setInitialListeners();
-    var listenToReduceCv = Object.create(reduceCv);
-    listenToReduceCv.setInitialListeners();
+            $('.header').addClass("blog--header");
+        }
+        var listenToGithub = Object.create(github);
+        listenToGithub.setInitialListeners();
+        var listenToBlog = Object.create(blog);
+        listenToBlog.buildFirstPost();
+        listenToBlog.setInitialListeners();
+        var listenToReduceCv = Object.create(reduceCv);
+        listenToReduceCv.setInitialListeners();
 
-    var listenToSocialMedia = Object.create(socialMedia);
-    listenToSocialMedia.setInitialListeners();
+        var listenToSocialMedia = Object.create(socialMedia);
+        listenToSocialMedia.setInitialListeners();
 
-    var listenToCV = Object.create(cv);
-    listenToCV.setInitialListeners();
-    twttr.widgets.load();
+        var listenToCV = Object.create(cv);
+        listenToCV.setInitialListeners();
+        twttr.widgets.load();
 
-  })
+    })
 
 })();
